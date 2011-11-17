db152512980c3ac00905a37808e32826c7073c98
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-17 22:05:50 +0000

    bugfix release 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/tags/1.0.1@464377 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/client-clock.js
@@ -0,0 +1,19 @@
+// Don't wait for DOM ready.
+var now = new Date();
+jQuery.post(
+	eme_ajax.ajaxurl, {
+	action: 'client_clock_submit',
+	client_unixtime: Math.round(now.getTime() / 1000), // make seconds
+	client_seconds: now.getSeconds(),
+	client_minutes: now.getMinutes(),
+	client_hours: now.getHours(),
+	client_wday: now.getDay(),
+	client_mday: now.getDate(),
+	client_month: now.getMonth()+1, // make 1-12
+	client_fullyear: now.getFullYear() },  
+	function(ret) {
+		if (ret == '1') {
+			top.location.href = self.location.href;
+		}
+	}
+);
