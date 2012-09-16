14d848208297abd105852510e4140dc94bbd8421
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-16 21:07:15 +0000

    release 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/tags/1.0.3@599809 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ eme_categories.php
@@ -0,0 +1,253 @@
+<?php
+function eme_categories_page() {      
+   global $wpdb;
+   
+   admin_show_warnings();
+   if (!current_user_can( get_option('eme_cap_categories')) && (isset($_GET['action']) || isset($_POST['action']))) {
+      $message = __('You have no right to update categories!','eme');
+      eme_categories_table_layout($message);
+   } elseif (isset($_GET['action']) && $_GET['action'] == "editcat") { 
+      // edit category  
+      eme_categories_edit_layout();
+   } else {
+      // Insert/Update/Delete Record
+      $categories_table = $wpdb->prefix.CATEGORIES_TBNAME;
+      $validation_result = '';
+      if (isset($_POST['action']) && $_POST['action'] == "edit" ) {
+         // category update required  
+         $category = array();
+         $category['category_name'] = trim(stripslashes($_POST['category_name']));
+         $validation_result = $wpdb->update( $categories_table, $category, array('category_id' => intval($_POST['category_ID'])) );
+      } elseif ( isset($_POST['action']) && $_POST['action'] == "add" ) {
+         // Add a new category
+         $category = array();
+         $category['category_name'] = trim(stripslashes($_POST['category_name']));
+         $validation_result = $wpdb->insert($categories_table, $category);
+      } elseif ( isset($_POST['action']) && $_POST['action'] == "delete" ) {
+         // Delete category or multiple
+         $categories = $_POST['categories'];
+         if (is_array($categories)) {
+            //Make sure the array is only numbers
+            foreach ($categories as $cat_id) {
+               if (is_numeric($cat_id)) {
+                  $cats[] = "category_id = $cat_id";
+               }
+            }
+            //Run the query if we have an array of category ids
+            if (count($cats > 0)) {
+               $validation_result = $wpdb->query( "DELETE FROM $categories_table WHERE ". implode(" OR ", $cats) );
+            } else {
+               $validation_result = false;
+               $message = __("Couldn't delete the categories. Incorrect category IDs supplied. Please try again.","eme");
+            }
+         }
+      }
+      //die(print_r($_POST));
+      if (is_numeric($validation_result) ) {
+         $message = (isset($message)) ? $message : __("Successfully {$_POST['action']}ed category", "eme");
+         eme_categories_table_layout($message);
+      } elseif ( $validation_result === false ) {
+         $message = (isset($message)) ? $message : __("There was a problem {$_POST['action']}ing your category, please try again.");                     
+         eme_categories_table_layout($message);
+      } else {
+         // no action, just a categories list
+         eme_categories_table_layout();   
+      }
+   }
+} 
+
+function eme_categories_table_layout($message = "") {
+   $categories = eme_get_categories();
+   $destination = admin_url("admin.php?page=eme-categories"); 
+   $table = "
+      <div class='wrap nosubsub'>\n
+         <div id='icon-edit' class='icon32'>
+            <br />
+         </div>
+         <h2>".__('Categories', 'eme')."</h2>\n ";   
+         
+         if($message != "") {
+            $table .= "
+            <div id='message' class='updated fade below-h2' style='background-color: rgb(255, 251, 204);'>
+               <p>$message</p>
+            </div>";
+         }
+         
+         $table .= "
+         <div id='col-container'>
+         
+            <?-- begin col-right -->
+            <div id='col-right'>
+             <div class='col-wrap'>
+                <form id='bookings-filter' method='post' action='".$destination."'>
+                  <input type='hidden' name='action' value='delete'/>";
+                  if (count($categories)>0) {
+                     $table .= "<table class='widefat'>
+                        <thead>
+                           <tr>
+                              <th class='manage-column column-cb check-column' scope='col'><input type='checkbox' class='select-all' value='1'/></th>
+                              <th>".__('ID', 'eme')."</th>
+                              <th>".__('Name', 'eme')."</th>
+                           </tr>
+                        </thead>
+                        <tfoot>
+                           <tr>
+                              <th class='manage-column column-cb check-column' scope='col'><input type='checkbox' class='select-all' value='1'/></th>
+                              <th>".__('ID', 'eme')."</th>
+                              <th>".__('Name', 'eme')."</th>
+                           </tr>
+                        </tfoot>
+                        <tbody>";
+                     foreach ($categories as $this_category) {
+                        $table .= "    
+                           <tr>
+                           <td><input type='checkbox' class ='row-selector' value='".$this_category['category_id']."' name='categories[]'/></td>
+                           <td><a href='".admin_url("admin.php?page=eme-categories&amp;action=editcat&amp;category_ID=".$this_category['category_id'])."'>".$this_category['category_id']."</a></td>
+                           <td><a href='".admin_url("admin.php?page=eme-categories&amp;action=editcat&amp;category_ID=".$this_category['category_id'])."'>".$this_category['category_name']."</a></td>
+                           </tr>
+                        ";
+                     }
+                     $table .= "
+                        </tbody>
+                     </table>
+   
+                     <div class='tablenav'>
+                        <div class='alignleft actions'>
+                        <input class='button-secondary action' type='submit' name='doaction2' value='Delete'/>
+                        <br class='clear'/>
+                        </div>
+                        <br class='clear'/>
+                     </div>";
+                  } else {
+                        $table .= "<p>".__('No categories have been inserted yet!', 'eme');
+                  }
+                   $table .= "
+                  </form>
+               </div>
+            </div> 
+            <?-- end col-right -->
+            
+            <?-- begin col-left -->
+            <div id='col-left'>
+            <div class='col-wrap'>
+                  <div class='form-wrap'>
+                     <div id='ajax-response'/>
+                  <h3>".__('Add category', 'eme')."</h3>
+                      <form name='add' id='add' method='post' action='".$destination."' class='add:the-list: validate'>
+                        <input type='hidden' name='action' value='add' />
+                         <div class='form-field form-required'>
+                           <label for='category_name'>".__('Category name', 'eme')."</label>
+                           <input id='category-name' name='category_name' id='category_name' type='text' value='' size='40' />
+                            <p>".__('The name of the category', 'eme').".</p>
+                         </div>
+                         <p class='submit'><input type='submit' class='button' name='submit' value='".__('Add category', 'eme')."' /></p>
+                      </form>
+                 </div>
+               </div>
+            </div>
+            <?-- end col-left -->
+         </div>
+   </div>";
+   echo $table;  
+}
+
+function eme_categories_edit_layout($message = "") {
+   $category_id = intval($_GET['category_ID']);
+   $category = eme_get_category($category_id);
+   $layout = "
+   <div class='wrap'>
+      <div id='icon-edit' class='icon32'>
+         <br />
+      </div>
+         
+      <h2>".__('Edit category', 'eme')."</h2>";   
+      
+      if($message != "") {
+         $layout .= "
+      <div id='message' class='updated fade below-h2' style='background-color: rgb(255, 251, 204);'>
+         <p>$message</p>
+      </div>";
+      }
+      $layout .= "
+      <div id='ajax-response'></div>
+
+      <form name='editcat' id='editcat' method='post' action='".admin_url("admin.php?page=eme-categories")."' class='validate'>
+      <input type='hidden' name='action' value='edit' />
+      <input type='hidden' name='category_ID' value='".$category['category_id']."'/>";
+      
+      $layout .= "
+         <table class='form-table'>
+            <tr class='form-field form-required'>
+               <th scope='row' valign='top'><label for='category_name'>".__('Category name', 'eme')."</label></th>
+               <td><input name='category_name' id='category-name' type='text' value='".eme_sanitize_html($category['category_name'])."' size='40'  /><br />
+                 ".__('The name of the category', 'eme')."</td>
+            </tr>
+         </table>
+      <p class='submit'><input type='submit' class='button-primary' name='submit' value='".__('Update category', 'eme')."' /></p>
+      </form>
+         
+      
+   </div>
+         
+   ";  
+   echo $layout;
+}
+
+function eme_get_categories($eventful=false,$scope="future",$extra_conditions=""){
+   global $wpdb;
+   $categories_table = $wpdb->prefix.CATEGORIES_TBNAME; 
+   $categories = array();
+   $orderby = " ORDER BY category_name ASC";
+   if ($eventful) {
+      $events = eme_get_events(0, $scope, "ASC");
+      if ($events) {
+         foreach ($events as $event) {
+            if (!empty($event['event_category_ids'])) {
+               $event_cats=explode(",",$event['event_category_ids']);
+               if (!empty($event_cats)) {
+                  foreach ($event_cats as $category_id) {
+                     $categories[$category_id]=$category_id;
+                  }
+               }
+            }
+         }
+      }
+      if (!empty($categories)) {
+         $event_cats=join(",",$categories);
+         if ($extra_conditions !="")
+            $extra_conditions = " AND ($extra_conditions)";
+         return $wpdb->get_results("SELECT * FROM $categories_table where category_id in ($event_cats) $extra_conditions $orderby", ARRAY_A);
+      }
+   } else {
+      if ($extra_conditions !="")
+         $extra_conditions = " WHERE ($extra_conditions)";
+      return $wpdb->get_results("SELECT * FROM $categories_table $extra_conditions $orderby", ARRAY_A);
+   }
+}
+
+function eme_get_category($category_id) { 
+   global $wpdb;
+   $categories_table = $wpdb->prefix.CATEGORIES_TBNAME; 
+   $sql = "SELECT * FROM $categories_table WHERE category_id ='$category_id'";   
+   $category = $wpdb->get_row($sql, ARRAY_A);
+   return $category;
+}
+
+function eme_get_event_categories($event_id) { 
+   global $wpdb;
+   $event_table = $wpdb->prefix.EVENTS_TBNAME; 
+   $categories_table = $wpdb->prefix.CATEGORIES_TBNAME; 
+   $sql = "SELECT category_name FROM $categories_table, $event_table where event_id ='$event_id' AND FIND_IN_SET(category_id,event_category_ids)";
+   $category = $wpdb->get_col($sql);
+   return $category;
+}
+
+function eme_get_location_categories($location_id) { 
+   global $wpdb;
+   $locations_table = $wpdb->prefix.LOCATIONS_TBNAME; 
+   $categories_table = $wpdb->prefix.CATEGORIES_TBNAME; 
+   $sql = "SELECT category_name FROM $categories_table, $locations_table where location_id ='$location_id' AND FIND_IN_SET(category_id,location_category_ids)";
+   $category = $wpdb->get_col($sql);
+   return $category;
+}
+?>
---
+++ extras/plugins/events-made-easy-frontend-submit/emefs.js
@@ -0,0 +1,99 @@
+var emefs_page = 0;
+var emefs_autocomplete_url = "/wp-content/plugins/events-manager-extended/locations-search.php";
+var emefs_gmap_enabled = 1;
+var emefs_gmap_hasSelectedLocation = 0;
+
+var emefs_autocomplete_options = {
+	width: 260,
+	selectFirst: false,
+	formatItem: function(row) {
+		item = eval("(" + row + ")");
+		return htmlDecode(item.name)+'<br /><small>'+htmlDecode(item.address)+' - '+htmlDecode(item.town)+ '</small>';
+	},
+	formatResult: function(row) {
+		item = eval("(" + row + ")");
+		return htmlDecode(item.name);
+	} 
+};
+
+function htmlDecode(value){
+	return jQuery('<div/>').html(value).text(); 
+}
+
+function emefs_deploy() {
+
+	jQuery("input#location_name").autocomplete(emefs_autocomplete_url, emefs_autocomplete_options);
+	
+	jQuery('input#location_name').result(function(event,data,formatted) {
+		item = eval("(" + data + ")"); 
+		jQuery('input#location_address').val(item.address);
+		jQuery('input#location_town').val(item.town);
+		emefs_displayAddress();
+	});
+	
+	jQuery("#event_start_date, #event_end_date").datepicker({ dateFormat: 'yy-mm-dd' });
+	jQuery('#event_start_time, #event_end_time').timeEntry({ hourText: 'Hour', minuteText: 'Minute', show24Hours: true, spinnerImage: '' });
+	
+	if(emefs_gmap_hasSelectedLocation){
+		emefs_displayAddress();
+	}
+	
+}
+
+function emefs_displayAddress(){
+	if(emefs_gmap_enabled) {
+	   eventLocation = jQuery("input#location_name").val(); 
+	   eventTown = jQuery("input#location_town").val(); 
+	   eventAddress = jQuery("input#location_address").val();
+	   emefs_loadMap(eventLocation, eventTown, eventAddress);
+	}
+}
+
+function emefs_loadMap(location, town, address){
+
+	var emefs_mapCenter = new google.maps.LatLng(-34.397, 150.644);
+	var emefs_map = false;
+	
+	var emefs_mapOptions = {
+		zoom: 12,
+		center: emefs_mapCenter,
+		scrollwheel: true,
+		disableDoubleClickZoom: true,
+		mapTypeControlOptions: {
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE]
+		},
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+
+	var emefs_geocoder = new google.maps.Geocoder();
+	
+	if (address !="") {
+		searchKey = address + ", " + town;
+	} else {
+		searchKey =  location + ", " + town;
+	}
+	
+	emefs_geocoder.geocode({'address': searchKey}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			jQuery("#event-map").slideDown('fast',function(){
+				if(!emefs_map){
+					var emefs_map = new google.maps.Map(document.getElementById("event-map"), emefs_mapOptions);
+				}
+				emefs_map.setCenter(results[0].geometry.location);
+				var emefs_marker = new google.maps.Marker({
+					map: emefs_map,
+					position: results[0].geometry.location
+				});
+				var emefs_infowindow = new google.maps.InfoWindow({
+					content: '<strong>' + location +'</strong><p>' + address + '</p><p>' + town + '</p>'
+	      		});
+				emefs_infowindow.open(emefs_map,emefs_marker);
+				jQuery('input#location_latitude').val(results[0].geometry.location.lat());
+				jQuery('input#location_longitude').val(results[0].geometry.location.lng());
+			});
+		} else {
+			jQuery("#event-map").slideUp();
+		}
+	});
+	
+}
