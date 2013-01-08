1f48188f6c25540ca0567a939ec2b4b2d170e1c0
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-08 20:52:31 +0000

    release of version 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/trunk@649886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- events-manager.php
+++ events-manager.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Events Made Easy
-Version: 1.0.5
+Version: 1.0.6
 Plugin URI: http://www.e-dynamics.be/wordpress
 Description: Description: Manage and display events. Includes recurring events; locations; widgets; Google maps; RSVP; ICAL and RSS feeds; Paypal. <a href="admin.php?page=eme-options">Settings</a> | <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=SMGDS4GLCYWNG&lc=BE&item_name=To%20support%20development%20of%20EME&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted">Donate</a>
 Author: Franky Van Liedekerke
@@ -12,6 +12,8 @@ Author URI: http://www.e-dynamics.be/
 Copyright (c) 2009, Davide Benini.  $Revision: 1 $
 Copyright (c) 2010, Franky Van Liedekerke.
 Copyright (c) 2011, Franky Van Liedekerke.
+Copyright (c) 2012, Franky Van Liedekerke.
+Copyright (c) 2013, Franky Van Liedekerke.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
