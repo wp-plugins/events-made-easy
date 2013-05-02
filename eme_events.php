de893478480826f9b25ee7d8248ec410188b95fd
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-02 09:54:12 +0000

    show no events message when no locations have been found if using /town/...
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/trunk@706860 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- eme_events.php
+++ eme_events.php
@@ -529,7 +529,11 @@ function eme_events_page_content() {
       $event_list_format_header = ( $event_list_format_header != '' ) ?  $event_list_format_header : "<ul class='eme_events_list'>";
       $event_list_format_footer = get_option('eme_event_list_item_format_footer' );
       $event_list_format_footer = ( $event_list_format_footer != '' ) ?  $event_list_format_footer : "</ul>";
-      $page_body = $event_list_format_header . eme_get_events_list ( get_option('eme_event_list_number_items' ), "future", "ASC", $stored_format, 0, '','',0,'','',0,$location_ids) .  $event_list_format_footer;
+      if (count($location_ids)>0) {
+         $page_body = $event_list_format_header . eme_get_events_list ( get_option('eme_event_list_number_items' ), "future", "ASC", $stored_format, 0, '','',0,'','',0,$location_ids) .  $event_list_format_footer;
+      } else {
+         $page_body = $event_list_format_header . get_option('eme_no_events_message') .  $event_list_format_footer;
+      }
       return $page_body;
    }
    if (isset ( $wp_query->query_vars['location_id'] ) && $wp_query->query_vars['location_id'] != '') {
