72cbdde88e5a94aa653d7ae620e154143e59d356
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-15 12:47:01 +0000

    make #_SPACESxx also work in RSVP info, next to #_RESPSPACES
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/trunk@822426 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- eme_rsvp.php
+++ eme_rsvp.php
@@ -1001,15 +1001,15 @@ function eme_replace_booking_placeholders($format, $booking, $target="html") {
             $replacement = apply_filters('eme_general', $replacement); 
          else 
             $replacement = apply_filters('eme_general_rss', $replacement); 
-      } elseif (preg_match('/#_RESPSPACES(.+)|#_SPACES(.+)|#_BOOKEDSEATS(.+)/', $result, $matches)) {
-         $field_id = intval($matches[1])-1;
+      } elseif (preg_match('/#_(RESPSPACES|SPACES|BOOKEDSEATS)(.+)/', $result, $matches)) {
+         $field_id = intval($matches[2])-1;
          if ($field_id<0) $field_id=0;
          if (eme_is_multiprice($booking['booking_price'])) {
              $seats=preg_split("/\|\|/",$booking['booking_seats_mp']);
              if (array_key_exists($field_id,$seats))
                 $replacement = $seats[$field_id];
          }
-      } elseif (preg_match('/#_RESPSPACES$|#_SPACES$|#_BOOKEDSEATS/', $result)) {
+      } elseif (preg_match('/#_RESPSPACES$|#_SPACES$|#_BOOKEDSEATS$/', $result)) {
          $replacement = $booking['booking_seats'];
       } elseif (preg_match('/#_USER_(RESERVEDSPACES|BOOKEDSEATS)$/', $result)) {
          $replacement = $booking['booking_seats'];
