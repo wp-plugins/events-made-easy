089f14dcc114c04e3cc6d8b05a8f60ffd81eb504
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-12 08:17:02 +0000

    added #_TOTALSEATSxx, #_AVAILABLESEATSxx and #_BOOKEDSEATSxx
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/trunk@837005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- events-manager.php
+++ events-manager.php
@@ -1146,14 +1146,41 @@ function eme_replace_placeholders($format, $event="", $target="html") {
             $replacement = eme_get_available_seats($event['event_id']);
          }
 
+      } elseif (preg_match('/#_(AVAILABLESPACES|AVAILABLESEATS)(.+)/', $result, $matches)) {
+         $field_id = intval($matches[2])-1;
+         if ($field_id<0) $field_id=0;
+         if (eme_is_multi($event['event_seats'])) {
+            $seats=eme_get_available_multiseats($event['event_id']);
+            if (array_key_exists($field_id,$seats))
+               $replacement = $seats[$field_id];
+         }
+
       } elseif ($event && preg_match('/#_(TOTALSPACES|TOTALSEATS)$/', $result)) {
          if ($rsvp_is_active && $event['event_rsvp']) {
             $replacement = $event['event_seats'];
          }
 
+      } elseif (preg_match('/#_(TOTALSPACES|TOTALSEATS)(.+)/', $result, $matches)) {
+         $field_id = intval($matches[2])-1;
+         if ($field_id<0) $field_id=0;
+         if (eme_is_multi($event['event_seats'])) {
+            $seats = preg_split("/\|\|/",$event['event_seats']);
+            if (array_key_exists($field_id,$seats))
+               $replacement = $seats[$field_id];
+         }
+
       } elseif ($event && preg_match('/#_(RESERVEDSPACES|BOOKEDSEATS)$/', $result)) {
          if ($rsvp_is_active && $event['event_rsvp']) {
             $replacement = eme_get_booked_seats($event['event_id']);
+         }
+
+      } elseif (preg_match('/#_(RESERVEDSPACES|BOOKEDSEATS)(.+)/', $result, $matches)) {
+         $field_id = intval($matches[2])-1;
+         if ($field_id<0) $field_id=0;
+         if (eme_is_multi($event['event_seats'])) {
+            $seats=eme_get_booked_multiseats($event['event_id']);
+            if (array_key_exists($field_id,$seats))
+               $replacement = $seats[$field_id];
          }
 
       } elseif ($event && preg_match('/#_USER_(RESERVEDSPACES|BOOKEDSEATS)$/', $result)) {
--- readme.txt
+++ readme.txt
@@ -68,6 +68,7 @@ See the FAQ section at [the documentation site](http://www.e-dynamics.be/wordpre
   This renders the use of the 'format' parameter obsolete and is more powerfull as it allows conditional tags inside the format template.
 * Feature: recurrence is now also possible on specific days, not just a pattern
 * Feature: maptype for google maps can be defined in the settings for global and individual maps (Roadmap, Satellite, Hybrid or Terrain)
+* Feature: added #_TOTALSEATSxx, #_AVAILABLESEATSxx and #_BOOKEDSEATSxx placeholders to show seat info per seat category
 * Improvement: remove some unneeded caching in the EME admin section.
 * Improvement: Dutch language update, thanks to Guido
 * Improvement: show which required fields are missing when filling out a rsvp form
