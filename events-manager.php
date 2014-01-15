a43faf428323e6bc74087fc6216a26df0d2c98ed
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-15 23:14:36 +0000

    release 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/trunk@839342 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- events-manager.php
+++ events-manager.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Events Made Easy
-Version: 1.2.2
+Version: 1.2.3
 Plugin URI: http://www.e-dynamics.be/wordpress
 Description: Description: Manage and display events. Includes recurring events; locations; widgets; Google maps; RSVP; ICAL and RSS feeds; Paypal, 2Checkout and Google Checkout. <a href="admin.php?page=eme-options">Settings</a> | <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=SMGDS4GLCYWNG&lc=BE&item_name=To%20support%20development%20of%20EME&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted">Donate</a>
 Author: Franky Van Liedekerke
