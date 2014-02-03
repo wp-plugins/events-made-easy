1fabfe8bd3f1199ad2f3edbb37dc7e68d4a6bdb4
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-03 22:46:24 +0000

    the booking price is now shown correctly as floating point
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/trunk@850900 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- eme_rsvp.php
+++ eme_rsvp.php
@@ -18,9 +18,7 @@ function eme_payment_form($event,$booking_id) {
    if (is_array($event)) {
       $total_price=eme_get_total_booking_price($event,$booking);
       $ret_string = "<div id='eme-rsvp-message' class='eme-rsvp-message'>".__('Payment handling','eme')."</div>";
-      // we use %s for the price format too, to avoid trailing deciman zeros 
-      // %d doesn't work, and %f adds trailing zeros
-      $ret_string .= sprintf(__("The booking price in %s is: %s",'eme'),$event['currency'],$total_price);
+      $ret_string .= sprintf(__("The booking price in %s is: %01.2f",'eme'),$event['currency'],$total_price);
       if ($event['use_paypal'])
          $ret_string .= eme_paypal_form($event,$booking_id);
       if ($event['use_2co'])
