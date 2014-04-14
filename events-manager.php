4e78c640a34bf2cc4f48489c28d003280dd1fdfe
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-14 18:02:26 +0000

    empty var init
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/trunk@893071 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- events-manager.php
+++ events-manager.php
@@ -1797,7 +1797,7 @@ function sort_stringlenth($a,$b){
    return strlen($b)-strlen($a);
 }
 
-function eme_trans_sanitize_html( $value, $lang, $do_convert=1 ) {
+function eme_trans_sanitize_html( $value, $lang='', $do_convert=1 ) {
    $value = eme_translate( $value,$lang);
    if ($do_convert) {
       return eme_sanitize_html($value);
