b28f51038f63b8897128afc6f61586f109eb6a2b
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-05 17:56:57 +0000

    small setting text update
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/tags/1.4.1@908376 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- eme_settings.php
+++ eme_settings.php
@@ -545,7 +545,7 @@ function eme_options_page() {
 <h3><?php _e ( 'RSVP: form format', 'eme' ); ?></h3>
 <table class='form-table'>
    <?php
-      eme_options_textarea (__('Form format','eme'),'eme_registration_form_format', __("The look and feel of the form for registrations. #_NAME, #_EMAIL and #_SEATS are obligated fields, if not present then the form will not be shown. Use #_FIELD1, #_FIELD2, ... for own defined fields.",'eme')  .'<br/>'.__('For more information about form fields, see ', 'eme')."<a target='_blank' href='http://www.e-dynamics.be/wordpress/?cat=44'>".__('the documentation', 'eme').'</a>');
+      eme_options_textarea (__('Form format','eme'),'eme_registration_form_format', __("The look and feel of the form for registrations. #_NAME, #_EMAIL and #_SEATS are obligated fields, if not present then the form will not be shown.",'eme')  .'<br/>'.__('For more information about form fields, see ', 'eme')."<a target='_blank' href='http://www.e-dynamics.be/wordpress/?cat=44'>".__('the documentation', 'eme').'</a>');
       eme_options_textarea (__('Cancel form format','eme'),'eme_cancel_form_format', __("The look and feel of the cancel form for registrations. #_NAME and #_EMAIL are obligated fields, if not present then the form will not be shown.", 'eme').'<br/>'.__('For more information about form fields, see ', 'eme')."<a target='_blank' href='http://www.e-dynamics.be/wordpress/?cat=44'>".__('the documentation', 'eme').'</a>');
    ?>
 </table>
