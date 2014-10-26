b75e38fc7eef024a4d75c58dc20ad68ebce441b7
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-26 23:18:39 +0000

    multisite install improvements
    git-svn-id: https://plugins.svn.wordpress.org/events-made-easy/trunk@1014401 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- events-manager.php
+++ events-manager.php
@@ -327,14 +327,15 @@ function eme_install($networkwide) {
    if (function_exists('is_multisite') && is_multisite()) {
       // check if it is a network activation - if so, run the activation function for each blog id
       if ($networkwide) {
-         $old_blog = $wpdb->blogid;
+         //$old_blog = $wpdb->blogid;
          // Get all blog ids
          $blogids = $wpdb->get_col("SELECT blog_id FROM ".$wpdb->blogs);
          foreach ($blogids as $blog_id) {
             switch_to_blog($blog_id);
             _eme_install();
+            restore_current_blog();
          }
-         switch_to_blog($old_blog);
+         //switch_to_blog($old_blog);
          return;
       }  
    } 
